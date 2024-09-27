97e8071d0611f9d203fb50f5a37392783e8ffa1d
Craudin23 <claudianoteles9@gmail.com>
2024-09-27 09:41:29 -0300

    calculadora simples - primeiro commit
    testando funcionalidades do node

---
+++ calc.js
@@ -0,0 +1,9 @@
+let SomaFunc = require('./somar')
+let SubFunc =require('./sub')
+let MultFunc =require('./mult')
+let DivFunc =require('./div')
+
+console.log(SomaFunc(8,7))
+console.log(SubFunc(450,58))
+console.log(MultFunc(88,7))
+console.log(DivFunc(458,7))
